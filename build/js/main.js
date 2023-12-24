daa2042d3e2e10f7d67ba2ac309ac93a6ac35379
Alexandr <104072970+Dennchik@users.noreply.github.com>
2023-12-25 02:01:57 +0300

    25 декабря 2023

---
+++ build/js/main.js
@@ -0,0 +1,45 @@
+if (isMobile.any()) {
+	// const openSide = document.querySelector('.sidebar-left');
+	// const btns = openSide.querySelectorAll('.triggerBtn');
+	// for (let i = 0; i < btns.length; i++) {
+	// 	btns[i].addEventListener('click', () => {
+	// 		openSide.classList.toggle('_open');
+	// 		for (let i = 0; i < btns.length; i++) {
+	// 			btns[i].classList.toggle('_open');
+	// 		}
+	// 	});
+	// }
+}
+const mediaQuery = window.matchMedia('(max-width: 768px)');
+if (mediaQuery.matches) {
+	const openSide = document.querySelector('.sidebar-left');
+	const btns = openSide.querySelectorAll('.triggerBtn');
+	for (let i = 0; i < btns.length; i++) {
+		btns[i].addEventListener('click', () => {
+			openSide.classList.toggle('_open');
+			for (let i = 0; i < btns.length; i++) {
+				btns[i].classList.toggle('_open');
+			}
+		});
+	}
+}
+
+const arrowLinks = document.querySelectorAll('.menu-items__link');
+arrowLinks.forEach((arrowLink) => {
+	arrowLink.addEventListener("click", () => {
+		const opened_menu = arrowLink.closest('.header__top-nav').querySelector('._opened-menu');
+		_toggleMenu(arrowLink);
+		if (opened_menu && opened_menu !== arrowLink) {
+			_toggleMenu(opened_menu);
+		}
+	});
+});
+const _toggleMenu = (arrowLink) => {
+	if (arrowLink.classList.contains('_opened-menu')) {
+		arrowLink.classList.remove('_opened-menu');
+	} else {
+		arrowLink.classList.add('_opened-menu');
+	}
+};
+
+//# sourceMappingURL=main.js.map
